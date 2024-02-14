d50d4ec63d212e69c6c8353e9424c59a18c3e3dc
Akhil Das <akhildas@Akhils-MacBook-Pro.local>
2024-02-14 17:43:09 +0530

    Second commit

--- codingdojo.js
+++ codingdojo.js
@@ -8,6 +8,9 @@ const n = 7;
 function display1() {
     for (let i = 1; i <= n; i++) {
         let str = "";
+        for (let k = 1; k <= n - i; k++) {
+            str += "   ";
+        }
         for (let j = 1; j <= (i * 2) - 1; j++) {
             str += " * ";
         }
